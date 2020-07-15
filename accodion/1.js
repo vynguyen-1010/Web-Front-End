b187939262bfa1f722b9589cda3b9960fc98b4af
vynguyen-1010 <66165751+vynguyen-1010@users.noreply.github.com>
2020-07-15 21:16:09 +0700

    m

--- accodion/1.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('.content').slideUp();
-    $('.small-block h3').click(function (e) { 
-       //$('.content').slideToggle();
-        $('.content').slideUp();
-        $(this).next().slideToggle();
-        $(this).toggleClass('xanh');
-    });
-})
--- hieu ung/1.js
+++
@@ -1,3 +0,0 @@
-$(function() {
-    TweenMax.staggerFrom($('.anh'),0.5,{top: 100,opacity: 0}, 0.1)
-});
